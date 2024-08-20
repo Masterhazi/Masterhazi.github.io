eb70dcac30f9c31f59984d7f71e978e2aea586a7
Masterhazi <86478294+Masterhazi@users.noreply.github.com>
2024-08-20 18:14:10 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import vpn from '../assets/vpn.png'
 import copeople from '../assets/copeople.png'
@@ -6,7 +5,7 @@ import Footer from './Footer'
 
 const ProjectCard = ({ image, title, description, git, technologies }) => {
     return (
-        <div className="max-w-sm sm:max-w-sm md:max-w-sm bg-gray-900 border border-neutral-100 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="bg-gray-900 border border-neutral-100 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 max-w-xs sm:max-w-md md:max-w-lg">
             {title=='Snap Shot' && <a href="#">
                 <img className="w-full rounded-t-lg h-auto object-cover " src={vpn} alt="" />
             </a>}
@@ -35,24 +34,18 @@ const ProjectCard = ({ image, title, description, git, technologies }) => {
         </div>
     );
 };
-  
+
 const Projects = () => {
     return (
         <div className="bg-black">
-            <div className="flex flex-wrap gap-7 justify-center items-center m-12 p-12">
-                {project.map((item, index) => (
-                    <ProjectCard
-                        key={index}
-                        image={item.image}
-                        title={item.title}
-                        description={item.description}
-                        links={item.links}
-                        git={item.git}
-                        technologies={item.technologies}
-                    />
-                ))}
+            <div className="container mx-auto p-12 sm:p-6">
+                <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+                    {project.map((item, index) => (
+                        <ProjectCard key={index} {...item} />
+                    ))}
+                </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 }
@@ -60,18 +53,11 @@ const Projects = () => {
 
 export const project = [
     {
-        title: 'Cirrhosis Patient Survival Prediction',
-        description: 'An ongoing project where I am predicting survival outcomes for cirrhosis patients using clinical data. I am employing feature engineering, outlier handling, and data scaling to build predictive models with Scikit-learn.',
-        image: 'https://drive.google.com/file/d/1IfsFDeSX1T7hVcr-qUpWx-6oNJ34QehQ/view?usp=drive_link',
-        git: 'https://github.com/Masterhazi/Liver-Cirrhosis-patient-survival-prediction',
-        technologies: ['Python', 'Scikit-learn', 'Feature Engineering', 'Data Scaling']
-    },
-    {
-        title: 'AIDS Survival Prediction',
-        description: 'This project focused on predicting AIDS patient survival based on their treatment regimens. Using Python and AutoML, I developed a model with a 92% accuracy rate, comparing the effectiveness of monotherapy versus combination therapy.',
-        image: 'https://drive.google.com/file/d/1iFTeBHI81is63vbMwBPTzxJKFYjmx5xX/view?usp=drive_link',
-        git: 'https://github.com/Masterhazi/AIDS-Survival-Prediction',
-        technologies: ['Python', 'Pandas', 'AutoML', 'Machine Learning']
+        title: 'Beta-Lactam Resistance Analysis',
+        description: 'This project involved a comprehensive survey and statistical analysis of beta-lactam resistant bacteria in drinking water across Anantapur Urban. I utilized data analysis, statistics, and feature engineering to identify correlations between water sources and the presence of resistant bacteria.',
+        image: 'https://drive.google.com/file/d/1vdmulqHtfm19484JewUoOE5wnJrKgEA0/view?usp=drive_link',
+        git: 'https://github.com/Masterhazi/Beta-Lactam-Resistance-Analysis',
+        technologies: ['Python', 'Data Analysis', 'Statistics', 'Data Visualization']
     },
     {
         title: 'Phytochemical Composition Analysis',
@@ -81,11 +67,18 @@ export const project = [
         technologies: ['Python', 'Data Management', 'Data Analysis']
     },
     {
-        title: 'Beta-Lactam Resistance Analysis',
-        description: 'This project involved a comprehensive survey and statistical analysis of beta-lactam resistant bacteria in drinking water across Anantapur Urban. I utilized data analysis, statistics, and feature engineering to identify correlations between water sources and the presence of resistant bacteria.',
-        image: 'https://drive.google.com/file/d/1vdmulqHtfm19484JewUoOE5wnJrKgEA0/view?usp=drive_link',
-        git: 'https://github.com/Masterhazi/Beta-Lactam-Resistance-Analysis',
-        technologies: ['Python', 'Data Analysis', 'Statistics', 'Data Visualization']
+        title: 'AIDS Survival Prediction',
+        description: 'This project focused on predicting AIDS patient survival based on their treatment regimens. Using Python and AutoML, I developed a model with a 92% accuracy rate, comparing the effectiveness of monotherapy versus combination therapy.',
+        image: 'https://drive.google.com/file/d/1iFTeBHI81is63vbMwBPTzxJKFYjmx5xX/view?usp=drive_link',
+        git: 'https://github.com/Masterhazi/AIDS-Survival-Prediction',
+        technologies: ['Python', 'Pandas', 'AutoML', 'Machine Learning']
+    },
+    {
+        title: 'Cirrhosis Patient Survival Prediction',
+        description: 'An ongoing project where I am predicting survival outcomes for cirrhosis patients using clinical data. I am employing feature engineering, outlier handling, and data scaling to build predictive models with Scikit-learn.',
+        image: 'https://drive.google.com/file/d/1IfsFDeSX1T7hVcr-qUpWx-6oNJ34QehQ/view?usp=drive_link',
+        git: 'https://github.com/Masterhazi/Liver-Cirrhosis-patient-survival-prediction',
+        technologies: ['Python', 'Scikit-learn', 'Feature Engineering', 'Data Scaling']
     }
 ]
 
