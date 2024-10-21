275a7b37464918736899d72194f5affe17793992
Masterhazi <86478294+Masterhazi@users.noreply.github.com>
2024-10-21 17:17:38 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
         ref.current++;
         setText((prevText) => prevText + name[ref.current - 1]);
       }
-    }, 50);
+    }, 150);
 
     // --- Botpress Script Injection ---
     const loadBotpressScript = () => {
