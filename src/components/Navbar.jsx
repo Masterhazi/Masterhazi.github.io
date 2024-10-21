58deaf5911c05ea70dc0cc44894f362674b53e47
Masterhazi <86478294+Masterhazi@users.noreply.github.com>
2024-10-21 20:40:37 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
   }, [pathname]);
 
   return (
-    <nav className="bg-gray-800 bg-opacity-50 backdrop-blur-lg p-4 text-white fixed w-full z-10 top-0" style={{ cursor: 'default' }}>
+    <nav className="bg-white-800 bg-opacity-50 backdrop-blur-lg p-4 text-white fixed w-full z-10 top-0" style={{ cursor: 'default' }}>
       <div className="mx-auto text-center flex w-5/6 justify-between">
         
         <div className="hidden sm:flex space-x-4 items-center text-sm">
